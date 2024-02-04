1d5d804877c2ccef77b205ce73dcd0830286581a
Sanjeewa Rathnayake <srathnayake@ksturnpike.com>
2024-02-04 00:24:52 -0600

    added list of names

--- src/main/java/org/leetcode/git/EmptyMain.java
+++ src/main/java/org/leetcode/git/EmptyMain.java
@@ -1,7 +1,11 @@
 package org.leetcode.git;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class EmptyMain {
     public static void main(String[] args) {
         System.out.println("This is empty main method");
+        List<String> names = Arrays.asList("qwe","wer","qwe");
     }
 }
